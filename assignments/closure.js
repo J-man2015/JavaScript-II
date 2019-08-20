15e32013b28a91db998d4aad9b824d794b2a4e1e
Jordan <j.egrey97@gmail.com>
2019-08-20 16:49:32 -0400

    complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,8 +3,12 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+const activity = "Jumping";
 
-
+const gym = function(){
+      return activity
+}
+console.log(gym());
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
@@ -16,6 +20,10 @@ const counterMaker = () => {
   //      NOTE: This `counter` function, being nested inside `counterMaker`,
   //      "closes over" the `count` variable. It can "see" it in the parent scope!
   // 3- Return the `counter` function.
+  const count = 0;
+  const counter = function(){
+    return ++count;
+  }
 };
 // Example usage: const myCounter = counterMaker();
 // myCounter(); // 1
